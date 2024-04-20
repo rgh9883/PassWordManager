cd00de29ba54149881785bc846f9eb7f224206e9
RahulHegde11 <rahul8.hegde@gmail.com>
2024-04-20 15:06:14 -0400

    Finished Add and View Buttons

--- passwordManager.py
+++ passwordManager.py
@@ -1,7 +1,9 @@
 
 import tkinter as tk
 import customtkinter as ctk
+import pyperclip
 
+#Functions
 def addName():
     nameScreen = ctk.CTkInputDialog(text="Type in Where Password will be used", title="passName")
     name = nameScreen.get_input()
@@ -10,9 +12,35 @@ def addName():
 
 def addPassword(namePass):
     addScreen = ctk.CTkInputDialog(text="Type in the Password", title="password")
-    print(namePass, addScreen.get_input())
+    password = addScreen.get_input()
+    with open('passwords.txt', 'a') as f:
+        f.write(namePass + "-" + password + "\n")
+    options = getOptions()
+    viewMenu.configure(values=options)
 
-#MainApp Settings
+def getOptions():
+    options = []
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip().split("-")
+            options.append(data[0])
+    return options
+
+def viewPass():
+    name = viewMenu.get()
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip().split("-")
+            if data[0] == name:
+                textDisplay.configure(text=data[1])
+                pyperclip.copy(data[1])
+                pyperclip.paste()
+                
+                
+
+
+
+#MainApp
 ctk.set_appearance_mode("dark")
 
 app = ctk.CTk()
@@ -22,8 +50,18 @@ app.title("passwordManagerMain")
 title = ctk.CTkLabel(app, text="PassWord Manager", font=('Bold', 20))
 title.pack(padx=10,pady=10)
 
-addButton = ctk.CTkButton(app, text="Add Password", command=addName)
+addButton = ctk.CTkButton(app, text="Add Password", command=addName, text_color='black')
 addButton.pack()
+
+viewButton = ctk.CTkButton(app, text="View Password", command=viewPass, text_color='black')
+viewButton.pack()
+
+viewOptions = getOptions()
+viewMenu = ctk.CTkOptionMenu(app, values=viewOptions, dropdown_fg_color='#a2c4fa', text_color='black', dropdown_text_color='black')
+viewMenu.pack()
+
+textDisplay = ctk.CTkLabel(app, text="")
+textDisplay.pack()
 
 
 app.mainloop()
